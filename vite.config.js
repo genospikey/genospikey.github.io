db34f0b0331cde71e2ab34c8441cc8c36910d1f3
genospikey <jeremyreyes@gmail.com>
2023-03-27 11:14:42 -0500

    Updated project settings

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: 'https://genospikey.github.io/',
+  assetsDir:'https://genospikey.github.io/assets/',
   plugins: [vue()]
 })
